d39db79dea3db4f359b07bc440f7f81e8eea528f
guilh <guilherme.monteiro.23.11.1998@gmail.com>
2024-03-16 08:40:58 -0300

    /feature/Adicionando metodos de Consulta

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -5,6 +5,7 @@ import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
+import com.mysql.cj.log.Log;
 
 import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
@@ -19,11 +20,25 @@ import java.util.Map;
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
+        cadastrarProduto();
+        Long id = 1L;
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+
+        Produto p = produtoDao.buscarPorId(1L);
+        System.out.println(p.getPreco());
+
+        List<Produto> todos = produtoDao.buscarPorNomeDaCategoria("CELULARES");
+        todos.forEach(p2 -> System.out.println(p.getNome()));
+
+        BigDecimal precoDoProduto = produtoDao.buscarPrecoDoProdutoPorNome("Xiamoi Redmi");
+        System.out.println(precoDoProduto);
+    }
+
+    private static void cadastrarProduto() {
         Categoria celulares = new Categoria("CELULARES");
 
         Produto celular = new Produto("Xiamoi Redmi","Muito legal",new BigDecimal("800"), celulares);
-
-
 
 
         EntityManager em = JPAUtil.getEntityManager();
@@ -31,11 +46,19 @@ public class CadastroDeProduto {
         CategoriaDao categoriaDao = new CategoriaDao(em);
 
         em.getTransaction().begin();
+        em.persist(celulares);
+        celulares.setNome("XPTO");
 
-        categoriaDao.cadastrar(celulares);
-        produtoDao.cadastrar(celular);
 
         em.getTransaction().commit();
-        em.close();
+        em.clear();
+
+//        celulares = em.merge(celulares);
+//        celulares.setNome("1234");
+//        em.getTransaction().commit();
+
+//        em.remove(celulares);
+//        em.getTransaction().commit();
+//        em.flush();
     }
 }
